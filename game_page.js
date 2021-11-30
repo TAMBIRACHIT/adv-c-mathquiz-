6c568445390fe9f8aafd6dca0f6d2ae8f400b5bd
Coder_rachit81 <95204792+Coder519@users.noreply.github.com>
2021-11-30 10:59:16 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -33,3 +33,46 @@ function send() {
 question_turn = "player1";
 answer_turn = "player2";
 
+
+
+function check()
+{
+	get_answer = document.getElementById("input_check_box").value;
+	if(get_answer == actual_answer)	
+	{
+		if(answer_turn == "player1")
+		{
+			player1_score = player1_score +1;
+		    document.getElementById("player1_score").innerHTML = player1_score;
+		}
+		else 
+		{
+			player2_score = player2_score +1;
+		    document.getElementById("player2_score").innerHTML = player2_score;
+		}
+	}
+	
+	if(question_turn == "player1")
+	{
+		question_turn = "player2"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player2name ;
+	}
+	else 
+	{
+		question_turn = "player1"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player1name ;
+	}
+
+	if(answer_turn == "player1")
+	{
+		answer_turn = "player2"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2name ;
+	}
+	else {
+		answer_turn = "player1"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1name ;
+	}
+
+    document.getElementById("output").innerHTML = "";
+}
+
